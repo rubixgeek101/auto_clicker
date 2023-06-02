dff81a3846f0428e0089f8e4b02cf024b7e6ff4f
Macsen Casaus <macsencasaus@gmail.com>
2023-06-01 19:06:28 -0500

    added argument parser and removed time import

--- auto_clicker.py
+++ auto_clicker.py
@@ -1,9 +1,35 @@
+"""
+Clicks left mouse button every given interval
+"""
+
+
+#imports
 import pyautogui as pag
-import time
+import argparse
 
-print('Press Ctrl-C to exit', flush = True)
 
-while True:
-    pag.click(interval = 1)
+#argument parser
+parser = argparse.ArgumentParser(description = "Clicks left mouse button repeatedly.")
+
+parser.add_argument('-i', '--interval',
+                    metavar = 'N',
+                    type=float,
+                    help = 'The time (sec) between each click.',
+                    default = 1)
+
+args = parser.parse_args()
+
+
+n = args.interval
+
+print('Press Ctrl-C in the console to exit.', flush = True)
+
+
+#clicker
+try: 
+    while True:
+        pag.click(interval = n)
+except KeyboardInterrupt:
+    print('Done.')
 
 
